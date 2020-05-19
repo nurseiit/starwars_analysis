697f36e5c2a3af8559c7dbbca370967a8469ac58
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-05-19 22:35:47 +0600

    💄 Add images for episodes

---
+++ report/imgSources.js
@@ -0,0 +1,10 @@
+const imgSources = [
+  "https://resizing.flixster.com/Y6S4_6hxa3wSRts6Fg6D4SQPDEA=/206x305/v1.bTsxMjMwMzAzMztqOzE4NDU3OzEyMDA7NDgwOzcyMg",
+  "https://resizing.flixster.com/zX9vN6elOycUxSn7SFcQoUaMHlQ=/206x305/v1.bTsxMjMwMzAzNDtqOzE4NDU3OzEyMDA7NDgwOzcyNw",
+  "https://resizing.flixster.com/muGb1p8vffVxlPlnqgyr7HV8hhg=/206x305/v1.bTsxMjMwMzAzODtqOzE4NDU3OzEyMDA7NDgwOzcxMQ",
+  "https://resizing.flixster.com/eOgVXLzNslZ_Vw4_eCOhyL8K8T0=/206x305/v1.bTsxMjMwMzAzOTtqOzE4NDU3OzEyMDA7NDgwOzc1Ng",
+  "https://resizing.flixster.com/y9_OhvG2D3XxGop6f3hEqmzqIW0=/206x305/v1.bTsxMjMwMzA0MDtqOzE4NDU3OzEyMDA7NDgwOzc0Nw",
+  "https://resizing.flixster.com/AF4dWS0Qjb7GJnR6qJ2jq9z5xG4=/206x305/v1.bTsxMjMwMzA0MTtqOzE4NDU3OzEyMDA7NDgwOzc2Mw",
+];
+
+export default imgSources;
